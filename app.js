6c1b74f055cb9cde3e06b23f5e254804b71f3c34
MatiasBossa <matubossa@gmail.com>
2020-10-12 13:43:46 -0300

    Routeo, Views, Public

---
+++ app.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = express();
+const PORT = 3000;
+
+app.use(express.static('public'));
+
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/views/index.html');
+});
+
+
+app.listen(PORT, () => {
+    console.log("El servidor de monto en el puerto " + PORT);
+});
