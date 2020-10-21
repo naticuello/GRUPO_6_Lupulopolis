db64d4c9e7a1ad6a4cd4d420ac6946f9c230b5fb
MatiasBossa <matubossa@gmail.com>
2020-10-21 11:58:29 -0300

    Nuevas modifcaciones

--- app.js
+++ app.js
@@ -9,6 +9,10 @@ app.get('/', (req, res) => {
     res.sendFile(__dirname + '/views/index.html');
 });
 
+app.get('/productDetail', (req, res) => {
+    res.sendFile(__dirname + '/views/productDetail.html');
+});
+
 
 app.listen(PORT, () => {
     console.log("El servidor de monto en el puerto " + PORT);
